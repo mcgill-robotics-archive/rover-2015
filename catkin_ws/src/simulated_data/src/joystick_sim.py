d369d0a76b844c00563b43f66042bd3efd90048b
Miles Cranmer <miles.cranmer@gmail.com>
2014-11-26 23:35:14 -0500

    joystick simulator now alternates between three states of motion

--- catkin_ws/src/simulated_data/src/joystick_sim.py
+++ catkin_ws/src/simulated_data/src/joystick_sim.py
@@ -3,14 +3,16 @@
 from geometry_msgs.msg import Twist
 import rospy
 import math
-from std_msgs import Int8
+from std_msgs.msg import Int8
 
 
 def publish_twist_continuous():
     rospy.init_node('joy_sim', anonymous=False)
     publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     typePublisher = rospy.Publisher('cmd_motion',Int8,queue_size=10)
-    motionTypeF = 0.
+    motionFloat = 0.
+    motion = Int8()
+    motion.data = 0
     MAX_LIN_VEL = rospy.get_param('/controls/max_lin_vel', 0.1)
     joy_lin = -math.pi
     joy_ang = math.pi
@@ -18,20 +20,22 @@ def publish_twist_continuous():
         if joy_lin > math.pi:
             joy_lin = -math.pi
         
-
         v_body = math.sin(joy_lin)*MAX_LIN_VEL
         w_body = math.cos(joy_lin)*MAX_LIN_VEL
 
         joy_lin = joy_lin + math.radians(2)
-        motionType += 0.001
-        if motionType < 3:
-            typePublisher.publish(0) #ackermann
-        elif motionType < 4:
-            typePublisher.publish(1) #point
-        elif motionType < 5:
-            typePublisher.publish(2)
+        motionFloat += 0.1
+        if motionFloat < 3:
+            motion.data = 0
+            typePublisher.publish(motion) #ackermann
+        elif motionFloat < 4:
+            motion.data = 1
+            typePublisher.publish(motion) #point
+        elif motionFloat < 5:
+            motion.data = 2
+            typePublisher.publish(motion)
         else:
-            motionType = 0
+            motionFloat = 0
 
         twist = Twist()
 
@@ -39,6 +43,7 @@ def publish_twist_continuous():
         twist.angular.z = w_body
         
         rospy.loginfo(twist)
+        rospy.loginfo(motion)
 
         publisher.publish(twist)
 
