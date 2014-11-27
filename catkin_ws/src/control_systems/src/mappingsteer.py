aa650cd3b663d72ffb287b26cb7c9689130df73c
Miles Cranmer <miles.cranmer@gmail.com>
2014-11-27 04:07:56 -0500

    updated simulator, point/translational motion now options via topic '/cmd_motion'

--- catkin_ws/src/control_systems/src/mappingsteer.py
+++ catkin_ws/src/control_systems/src/mappingsteer.py
@@ -13,26 +13,34 @@ import math #for trig functions
 # smrv: starboard middle wheel rotation velocity
 # prrv: port rear wheel rotation velocity
 # srrv: starboard rear wheel rotation velocity
- 
 
 D = 50e-2  # distance between wheels of: front and middle/middle and rear[m]   
 B = 40e-2  # distance between longitudinal axis and port/startboard wheels[m]
 R = 16.5e-2 # wheel radius [m]
 W = 15e-2 # wheel width [m]
-T = math.pi/4 # max angle of front and rear wheels [rad]
+#This value needs to be tested for
+T = 3*math.pi/8 # max angle of front and rear wheels [rad] (technically no max)
 
+#angle on the wheels for point steering
+pointSteeringAngle = math.pi/2 - math.atan(B/D)
+#radius from the wheels to the middle of the rover for point steering
+pointSteeringRadius = math.sqrt(D**2+B**2)
 zero = 1e-10 # Offers protection against numbers very close to zero
 
 #the minimum distance of the IPCR that the wheels can accomodate
 rhoMin = D*math.tan(math.pi/2-T)+B
 
 #function returns the sign of a variable (1,0 or -1)
-def sign(n): return float(n)/abs(float(n))
+def sign(n): 
+	if n == 0:
+		return 0.  
+	else: 
+		return float(n)/abs(float(n))
 
 #this function maps joystick input to steering angle of 6 wheels
 #wBody is only always true in magnitude 
 #(except for when wheels cannot accomodate)
-def steer(vBody, wBody):er
+def steer(vBody, wBody):
 	#To be used in the final calculation of the velocity
 	sgnv = sign(vBody)
 	#angle of the wheels also depend on which way the rover is moving
@@ -98,9 +106,7 @@ def steer(vBody, wBody):er
 
 		#Simple trig to get angle to each wheel
 		pfsa = math.atan(D/(rho-B)) 
-		
 		sfsa = math.atan(D/(rho+B))
-
 		#incorporate the correct direction of the angular
 		#displacement of the wheels
 		#multiplying this by the sign of the velocity makes the angular
@@ -146,14 +152,14 @@ def pointTurn(wBody):
 	wBody = float(wBody)
 	movement = True
 	#wheels have specific angle - all of them should form a circle together
-	pfsa = math.pi/2 - math.atan(B/D) #forms circle
+	pfsa = pointSteeringAngle #forms circle
 	sfsa = -pfsa 
 	pmsa = 0
 	smsa = 0
 	prsa = -pfsa
 	srsa = pfsa
 
-	r = math.sqrt(D**2+B**2)
+	r = pointSteeringRadius
 	v = wBody*r #linear velocity of each wheel
 
 	pfrv = v/R #match angular velocity to rotation of wheel
@@ -162,6 +168,49 @@ def pointTurn(wBody):
 	smrv = 0
 	prrv = pfrv
 	srrv = -pfrv	
+	#I split them up to stay within 80 columns 
+	out={'movement':movement,'pfsa': pfsa,'sfsa': sfsa,'pmsa': pmsa}
+	#add more values
+	out.update({'smsa': smsa,'prsa': prsa,'srsa': srsa,'pfrv': pfrv})
+	out.update({'sfrv': sfrv,'pmrv': pmrv,'smrv': smrv,'prrv': prrv})
+	out.update({'srrv': srrv})
+	return out
+
+#translational motion will always make wheel move in forward direction
+def translationalMotion(y,x):
+	###########################
+	#eventually this function should pick the angle based on the 
+	#current and accumulated angles of the wheels
+	###########################
+	if abs(x) < zero:
+		#just forward/zero motion
+		return steer(y,0)
+		#determines which side should get the diagonal
+	sgnx = sign(x)
+	#equivalent direction of wheels
+	theta = math.pi/2 - math.atan(y/x)
+	#now find actual direction of wheels, such that
+	#the wheels will maintain a similar direction when
+	#the joystick is pointed right or left
+	if sgnx > 0 and theta < 0:#make theta positive
+		theta = math.pi - theta
+	elif sgnx <0 and theta > 0:#make theta negative
+		theta = theta - math.pi
+	movement = True
+	pfsa = theta
+	sfsa = theta
+	pmsa = 0 #wheels cannot steer
+	smsa = 0
+	prsa = theta
+	srsa = theta
+	#translate linear velocity to rotational velocity of wheel
+	#along with the correct direction
+	pfrv = max([abs(x),abs(y)])/R
+	sfrv = pfrv
+	pmrv = 0
+	smrv = 0
+	prrv = pfrv
+	srrv = pfrv
 	#I split them up to stay within 80 columns 
 	out={'movement':movement,'pfsa': pfsa,'sfsa': sfsa,'pmsa': pmsa}
 	#add more values
