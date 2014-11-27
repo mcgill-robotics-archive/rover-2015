29e2378fe40d3a6f8a431a4666f20c07444ca828
Miles Cranmer <miles.cranmer@gmail.com>
2014-11-26 23:15:17 -0500

    incorporating new topic for type of motion

--- catkin_ws/src/simulated_data/src/joystick_sim.py
+++ catkin_ws/src/simulated_data/src/joystick_sim.py
@@ -3,12 +3,14 @@
 from geometry_msgs.msg import Twist
 import rospy
 import math
+from std_msgs import Int8
 
 
 def publish_twist_continuous():
     rospy.init_node('joy_sim', anonymous=False)
     publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-
+    typePublisher = rospy.Publisher('cmd_motion',Int8,queue_size=10)
+    motionTypeF = 0.
     MAX_LIN_VEL = rospy.get_param('/controls/max_lin_vel', 0.1)
     joy_lin = -math.pi
     joy_ang = math.pi
@@ -21,7 +23,16 @@ def publish_twist_continuous():
         w_body = math.cos(joy_lin)*MAX_LIN_VEL
 
         joy_lin = joy_lin + math.radians(2)
-        
+        motionType += 0.001
+        if motionType < 3:
+            typePublisher.publish(0) #ackermann
+        elif motionType < 4:
+            typePublisher.publish(1) #point
+        elif motionType < 5:
+            typePublisher.publish(2)
+        else:
+            motionType = 0
+
         twist = Twist()
 
         twist.linear.x = v_body
