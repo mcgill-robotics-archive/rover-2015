be4b3c3896fd33f7e97c2f282c2e02b524b27276
David Lavoie-Boutin <d.lavoie.boutin@me.com>
2014-12-18 14:50:37 -0500

    cleaned odo header

--- catkin_ws/src/odometry/include/mw_odo.h
+++ catkin_ws/src/odometry/include/mw_odo.h
@@ -1,16 +1,7 @@
-/*
- * File: MW_odo.h
- *
- * MATLAB Coder version            : 2.7
- * C/C++ source code generated on  : 18-Dec-2014 12:35:00
- */
-
 #ifndef __MW_ODO_H__
 #define __MW_ODO_H__
+
 #include "odometry/DisplacementMiddleWheels.h"
-#include "ros/ros.h"
-#include "std_msgs/Float32.h"
-/* Function Declarations */
 
 double rLeft, rRight, whBase;
 
@@ -18,8 +9,3 @@ bool MW_odo(odometry::DisplacementMiddleWheels::Request  &req,
             odometry::DisplacementMiddleWheels::Response &res);
 #endif
 
-/*
- * File trailer for MW_odo.h
- *
- * [EOF]
- */
