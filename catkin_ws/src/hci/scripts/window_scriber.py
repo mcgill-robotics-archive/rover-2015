9bcd8a7c0da67c46294d217dadf5e338ef2132c8
Oscar <xu.shu@mail.mcgill.ca>
2014-12-19 14:58:24 -0500

    add camera feature

--- catkin_ws/src/hci/scripts/window_scriber.py
+++ catkin_ws/src/hci/scripts/window_scriber.py
@@ -24,42 +24,39 @@ from sensor_msgs.msg import Image
 class CentralUi(QtGui.QMainWindow):
 
     def __init__(self, parent=None):
-        super(CentralUi,self).__init__(parent)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.signal=QtCore.QTimer()
-        QtCore.QObject.connect(self.signal, QtCore.SIGNAL("timeout()"),self.checksignal)
-    	
-        self.input3=self.ui.Camera3Feed.currentText();
-
-        self.listener3()
-        self.signal.start(100)
-
+		super(CentralUi,self).__init__(parent)
+		self.image = None
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
+		self.signal=QtCore.QTimer(self)
+		self.signal.timeout.connect(self.checksignal)
+		self.listener3()
+		self.signal.start(100)
     
     def callback3(self, data):
-        try:
-            image = QtGui.QPixmap.fromImage(QtGui.QImage(data.data, data.width, data.height, QtGui.QImage.Format_RGB888))
-        finally:
-            pass
-        if image is not None:
-            self.ui.camera3.setPixmap(image)
-        else:
-            self.ui.camera3.setText("No video feed")
+		try:
+			self.image = data
+		finally:
+			pass
+		
 
     def checksignal(self):
-        if self.input3!=self.ui.Camera3Feed.currentText():
-            self.input3=self.ui.Camera3Feed.currentText()
-            self.listen.unregister()
-            if self.ui.Camera3Feed.currentText()=="feed 1":
-                self.listen=rospy.Subscriber('chatter1',String, self.callback)
-            if self.ui.Camera3Feed.currentText()=="feed 2":
-                self.listen=rospy.Subscriber('chatter2',String, self.callback)
-            if self.ui.Camera3Feed.currentText()=="feed n":
-                self.listen=rospy.Subscriber('chatter3',String, self.callback)
+		if self.image is not None:
+			try: 
+				qimage = QtGui.QImage(self.image.data, self.image.width/3, self.image.height/3, QtGui.QImage.Format_RGB32)
+				#TODO: FIGURE OUT THIS DIVIDED BY 3 !!! 
+				# wiskey-tango-foxtrot
+				image = QtGui.QPixmap.fromImage(qimage)		
+			finally:
+				pass
+			self.ui.camera3.setPixmap(image)
+		else:
+			self.ui.camera3.setText("no video feed")
+	
 
     def listener3(self):
         rospy.init_node('listener3',anonymous=False)
-        self.listen=rospy.Subscriber("chatter1",String, self.callback3)
+        self.listen=rospy.Subscriber("/camera_front_right/camera/image_raw",Image, self.callback3)
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
