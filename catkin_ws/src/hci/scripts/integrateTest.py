b91515c44e6de46758fbda6a7aa966f95f30a167
David Lavoie-Boutin <d.lavoie.boutin@me.com>
2015-01-12 22:33:21 -0500

    refactored image handlers to use ros parameters instead of constants and moved feed selection from timer to signal and slots system from piquet

--- catkin_ws/src/hci/scripts/integrateTest.py
+++ catkin_ws/src/hci/scripts/integrateTest.py
@@ -26,11 +26,18 @@ class CentralUi(QtGui.QMainWindow):
         self.ui.setupUi(self)
         self.controller = JoystickController()
 
-        self.image1=None
-        self.image2=None
-        self.image3=None
-        self.image4=None
-       
+        # feed 1 holders
+        self.__image1=None
+        self.__screen1Subscriber=None
+        # feed 2 holders
+        self.__image2=None
+        self.__screen2Subscriber=None
+        # feed 3 holders
+        self.__image3=None
+        self.__screen3Subscriber=None
+
+        ## List of topic names for the camera feeds, compiled from parameter server
+        self.camera_topic_list = []
 
         self.controller_timer = QtCore.QTimer()
         self.thrust_pub_timer = QtCore.QTimer()
@@ -39,29 +46,33 @@ class CentralUi(QtGui.QMainWindow):
         rospy.loginfo("Starting joystick acquisition")
 
         ##button connects
-
         # controller timer connect
-        QtCore.QObject.connect(self.controller_timer, QtCore.SIGNAL("timeout()"), self.update_test)
+        QtCore.QObject.connect(self.controller_timer, QtCore.SIGNAL("timeout()"), self.read_controller)
+        # joystick mode buttons signal connect
         QtCore.QObject.connect(self.ui.DriveMode, QtCore.SIGNAL("clicked()"), self.setMode0)
         QtCore.QObject.connect(self.ui.ArmBaseMode, QtCore.SIGNAL("clicked()"), self.setMode1)
         QtCore.QObject.connect(self.ui.EndEffectorMode, QtCore.SIGNAL("clicked()"), self.setMode2)
         QtCore.QObject.connect(self.ui.function4, QtCore.SIGNAL("clicked()"), self.setMode3)
+        # camera feed selection signal connects
+        QtCore.QObject.connect(self.ui.Camera1Feed, QtCore.SIGNAL("currentIndexChanged(int index)"), self.setFeed1Index)
+        QtCore.QObject.connect(self.ui.Camera2Feed, QtCore.SIGNAL("currentIndexChanged(int index)"), self.setFeed2Index)
+        QtCore.QObject.connect(self.ui.Camera3Feed, QtCore.SIGNAL("currentIndexChanged(int index)"), self.setFeed3Index)
 
-        self.signal=QtCore.QTimer(self)
-        self.signal.timeout.connect(self.repaint_image)
-        self.signal.timeout.connect(self.check_signal)
-        self.listener()
-        self.signal.start(100)
+        self.screen_redraw_timer=QtCore.QTimer(self)
+        self.screen_redraw_timer.timeout.connect(self.repaint_image)
+        self.screen_redraw_timer.start(100)
 
+        self.ros_init()
+        rospy.loginfo("HCI initialization completed")
 
-    def set_controller_timer(self):
-        if self.controller is not None:
+    def setControllerTimer(self):
+        if self.controller.controller is not None:
             self.controller_timer.start(misc_vars.controller_updateFrequency)
             rospy.loginfo("Started controller timer")
         else:
             rospy.loginfo("Missing controller, timer aborted")
 
-    def update_test(self):
+    def readController(self):
         self.controller.update()
         self.ui.MainX.setValue(self.controller.a1*1000)
         self.ui.MainY.setValue(-self.controller.a2*1000)
@@ -73,13 +84,13 @@ class CentralUi(QtGui.QMainWindow):
         elif self.controller.b4:
             self.ui.Camera1Feed.setCurrentIndex(4)
         elif self.controller.b7:
-            self.setMode(0)
+            self.setControllerMode(0)
         elif self.controller.b8:
-            self.setMode(1)
+            self.setControllerMode(1)
         elif self.controller.b9:
-            self.setMode(2)
+            self.setControllerMode(2)
         elif self.controller.b10:
-            self.setMode(3)
+            self.setControllerMode(3)
         elif self.controller.hat ==(-1,0):
             self.ui.Camera1Feed.setCurrentIndex(1)
         elif self.controller.hat ==(0,1):
@@ -88,32 +99,27 @@ class CentralUi(QtGui.QMainWindow):
             self.ui.Camera1Feed.setCurrentIndex(3)
         elif self.controller.hat ==(1,0):
             self.ui.Camera1Feed.setCurrentIndex(2)
-        self.controller.hat = (0,0)
-        self.controller.b2 = False
-        self.controller.b3 = False
-        self.controller.b4 = False
-        self.controller.b7 = False
-        self.controller.b8 = False
-        self.controller.b9 = False
-        self.controller.b10 = False
+
+        self.controller.clear_buttons()
+
 
     def setMode0(self):
-        self.setMode(0)
+        self.setControllerMode(0)
 
 
     def setMode1(self):
-        self.setMode(1)
+        self.setControllerMode(1)
 
 
     def setMode2(self):
-        self.setMode(2)
+        self.setControllerMode(2)
 
 
     def setMode3(self):
-        self.setMode(3)
+        self.setControllerMode(3)
 
 
-    def setMode(self, mode_id):
+    def setControllerMode(self, mode_id):
         if mode_id == 0:
             self.ui.DriveMode.setChecked(True)
             self.ui.ArmBaseMode.setChecked(False)
@@ -135,54 +141,62 @@ class CentralUi(QtGui.QMainWindow):
             self.ui.EndEffectorMode.setChecked(False)
             self.ui.function4.setChecked(True)
 
-    def callback3(self, data):
+
+    def callbackScreen3(self, data):
         try:
-            self.image3 = data
+            self.__image3 = data
         finally:
             pass
-    def callback2(self, data):
+
+
+    def callbackScreen2(self, data):
         try:
-            self.image2=data
+            self.__image2=data
         finally:
             pass
-    def callback1(self, data):
+
+
+    def callbackScreen1(self, data):
         try:
-            self.image1=data
+            self.__image1=data
         finally:
             pass
-    def callback4(self,data):
+
+
+    def setFeed1Index(self, newIndex):
         try:
-            self.image4=data
-        finally:
-            pass
-    
-    def check_signal(self):
-        if self.ui.Camera1Feed.currentIndex() != 0:
-            self.listen1.unregister()
-            self.listen1=rospy.Subscriber("/hello",Image,self.callback4)
-            self.image1=None
-        else:
-            self.listen1.unregister()
-            self.listen1=rospy.Subscriber("/camera_front_right/camera/image_raw",Image, self.callback1)
-        if self.ui.Camera2Feed.currentIndex() != 0:
-            self.listen2.unregister()
-            self.listen2=rospy.Subscriber("/hello",Image,self.callback4)
-            self.image2=None
-        else:
-            self.listen2.unregister()
-            self.listen2=rospy.Subscriber("/camera_front_right/camera/image_raw",Image, self.callback2)    
-        if self.ui.Camera3Feed.currentIndex() != 0:
-            self.listen3.unregister()
-            self.listen3=rospy.Subscriber("/hello",Image,self.callback4)
-            self.image3=None
-        else:
-            self.listen3.unregister()
-            self.listen3=rospy.Subscriber("/camera_front_right/camera/image_raw",Image, self.callback3)
+            topic = self.camera_topic_list[newIndex]
+        except IndexError :
+            rospy.logwarn("New camera feed topic name not found")
+            return
+        self.__screen1Subscriber.unsubscribe()
+        self.__screen1Subscriber = rospy.Subscriber(topic, Image, self.callbackScreen1)
+
+
+    def setFeed2Index(self, newIndex):
+        try:
+            topic = self.camera_topic_list[newIndex]
+        except IndexError :
+            rospy.logwarn("New camera feed topic name not found")
+            return
+        self.__screen2Subscriber.unsubscribe()
+        self.__screen2Subscriber = rospy.Subscriber(topic, Image, self.callbackScreen1)
+
+
+    def setFeed3Index(self, newIndex):
+        try:
+            topic = self.camera_topic_list[newIndex]
+        except IndexError :
+            rospy.logwarn("New camera feed topic name not found")
+            return
+        self.__screen3Subscriber.unsubscribe()
+        self.__screen3Subscriber = rospy.Subscriber(topic, Image, self.callbackScreen1)
+
 
     def repaint_image(self):
-        if self.image2 is not None:
+        if self.__image2 is not None:
             try:
-                qimage2 = QtGui.QImage(self.image2.data, self.image2.width/3, self.image2.height/3,QtGui.QImage.Format_RGB888)
+                qimage2 = QtGui.QImage(self.__image2.data, self.__image2.width/3, self.__image2.height/3,QtGui.QImage.Format_RGB888)
                 #TODO: FIGURE OUT THIS DIVIDED BY 3 !!! 
                 # wiskey-tango-foxtrot
                 image2 = QtGui.QPixmap.fromImage(qimage2)     
@@ -191,9 +205,9 @@ class CentralUi(QtGui.QMainWindow):
             self.ui.camera2.setPixmap(image2)
         else:
             self.ui.camera2.setText("no video feed")
-        if self.image3 is not None:
+        if self.__image3 is not None:
             try: 
-                qimage3 = QtGui.QImage(self.image3.data, self.image3.width/3, self.image3.height/3,QtGui.QImage.Format_RGB888)
+                qimage3 = QtGui.QImage(self.__image3.data, self.__image3.width/3, self.__image3.height/3,QtGui.QImage.Format_RGB888)
                 #TODO: FIGURE OUT THIS DIVIDED BY 3 !!! 
                 # wiskey-tango-foxtrot
                 image3 = QtGui.QPixmap.fromImage(qimage3)       
@@ -202,9 +216,9 @@ class CentralUi(QtGui.QMainWindow):
             self.ui.camera3.setPixmap(image3)
         else:
             self.ui.camera3.setText("no video feed")
-        if self.image1 is not None:
+        if self.__image1 is not None:
             try: 
-                qimage1 = QtGui.QImage(self.image1.data, self.image1.width/3, self.image1.height/3,QtGui.QImage.Format_RGB888)
+                qimage1 = QtGui.QImage(self.__image1.data, self.__image1.width/3, self.__image1.height/3,QtGui.QImage.Format_RGB888)
                 #TODO: FIGURE OUT THIS DIVIDED BY 3 !!! 
                 # wiskey-tango-foxtro
                 image1 = QtGui.QPixmap.fromImage(qimage1)     
@@ -215,17 +229,23 @@ class CentralUi(QtGui.QMainWindow):
             self.ui.camera1.setText("no video feed")
 
 
-
-    
-
-    def listener(self):
+    def ros_init(self):
         rospy.init_node('listener',anonymous=False)
-        self.listen3=rospy.Subscriber("/camera_front_right/camera/image_raw",Image, self.callback3)
-        self.listen2=rospy.Subscriber("/camera_front_right/camera/image_raw",Image, self.callback2)
-        self.listen1=rospy.Subscriber("/camera_front_right/camera/image_raw",Image, self.callback1)
-        
-        
-        
+        self.getImageTopic()
+        if len(self.camera_topic_list)>2:
+            self.__screen3Subscriber=rospy.Subscriber(self.camera_topic_list[2],Image, self.callback3)
+            self.__screen2Subscriber=rospy.Subscriber(self.camera_topic_list[1],Image, self.callback2)
+            self.__screen1Subscriber=rospy.Subscriber(self.camera_topic_list[0],Image, self.callback1)
+
+
+    def getImageTopic(self):
+        self.camera_topic_list.append(rospy.get_param("camera/topic_haz_front", "/hazcam_front/camera/image_raw"))
+        self.camera_topic_list.append(rospy.get_param("camera/topic_haz_left", "/hazcam_left/camera/image_raw"))
+        self.camera_topic_list.append(rospy.get_param("camera/topic_haz_right", "/hazcam_right/camera/image_raw"))
+        self.camera_topic_list.append(rospy.get_param("camera/topic_haz_back", "/hazcam_back/camera/image_raw"))
+        self.camera_topic_list.append(rospy.get_param("camera/topic_arm", "/camera_arm/camera/image_raw"))
+        self.camera_topic_list.append(rospy.get_param("camera/topic_pan_tilt", "/camera_pan_tilt/camera/image_raw"))
+
     #publisher for velocity
     #TODO change names once control systems has a defined topic name and variable names, copied from AUV as of now
     def publish_velocity(self):
