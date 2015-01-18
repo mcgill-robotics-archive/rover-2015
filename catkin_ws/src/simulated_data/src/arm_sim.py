b239a5bfabfa6b70394cd19256378105ea5319b1
Miles Cranmer <miles.cranmer@gmail.com>
2015-01-18 02:56:30 -0500

    Commented; made simulation movement more fluid

--- catkin_ws/src/simulated_data/src/arm_sim.py
+++ catkin_ws/src/simulated_data/src/arm_sim.py
@@ -11,28 +11,35 @@ def publish_arm_motion_continuous():
 	#object which publishes arm control data from joystick
 	armPublisher = rospy.Publisher('/cmd_arm', ArmMotion, queue_size=10)
 
-
+	#variable to be published
 	armSettings = ArmMotion()
+	#coordinates in x-y plane (0 to 1 (max))
 	armSettings.x = 0.
 	armSettings.y = 0.
+	#angle of x-y plane
 	armSettings.theta = 0.
 	armSettings.on = True
 	motionFloat = 0.
-
+	#step to be repeated
 	while not rospy.is_shutdown():
+		#cycle through
 		if motionFloat > 2*math.pi:
 			motionFloat = 0.
 		else:
 			motionFloat += 0.01
-
-		armSettings.x = 0.5*math.cos(motionFloat)+1
-		armSettings.y = 0.5*math.sin(motionFloat)+1
-		armSettings.theta = (motionFloat-math.pi)/4.
+		#x and y move slowly in a circle	
+		armSettings.x = 0.5*math.cos(motionFloat)+0.5
+		armSettings.y = 0.5*math.sin(motionFloat)+0.5
+		#theta just moves back and forth
+		if motionFloat < math.pi:
+			armSettings.theta = (motionFloat-math.pi/2.)/4.
+		else:
+			armSettings.theta = (3*math.pi/2-motionFloat)/4.
 
 		rospy.loginfo(armSettings)
 
 		armPublisher.publish(armSettings)
-
+		#60 Hz processing cycle
 		r = rospy.Rate(60)
 		r.sleep()
 
