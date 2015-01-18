53d764e69bc0481447e757307a193a5527452403
Miles Cranmer <miles.cranmer@gmail.com>
2015-01-17 22:50:49 -0500

    Added variable type to ros messages, created test file

--- catkin_ws/src/simulated_data/src/arm_sim.py
+++ catkin_ws/src/simulated_data/src/arm_sim.py
@@ -2,4 +2,10 @@
 #This program simulates control of a robotic arm, and publishes to 
 #the topic "cmd_arm"
 
-def publish_
+import rospy, math
+from control_systems.msg import ArmMotion
+
+def publish_arm_motion_continuous():
+	print "OREONO"
+
+publish_arm_motion_continuous()
