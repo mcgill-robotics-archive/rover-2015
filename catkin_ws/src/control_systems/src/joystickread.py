bec53fefa41e5bfb61ac9f3f2f96aec21d652443
mdtmc <miles.cranmer@gmail.com>
2015-01-20 16:07:03 -0500

    Finished adding ros capabilities to arm_read, both arm programs are now working

--- catkin_ws/src/control_systems/src/joystickread.py
+++ catkin_ws/src/control_systems/src/joystickread.py
@@ -57,7 +57,7 @@ class DualJoystickReader(object):
 		rospy.Subscriber('/cmd_motion',MotionType,self.update_value_motion,
 							queue_size=10)
 
-		#
+		#Secondary joystick
 		rospy.Subscriber('/cmd_alt_vel',Twist,self.update_second_joystick,
 							queue_size=10)
 		
@@ -174,28 +174,28 @@ class DualJoystickReader(object):
 	def run(self):
             #calculate required wheel angles, speeds
 		r = rospy.Rate(60)
-                #continue endlessly
-                while not rospy.is_shutdown():
-                    logMessage = String()
-                    #begin message
-                    logMessage.data = "\n________________________________________"
-                    logMessage.data += "\n\n\nINITIATE MESSAGE:\n\n\n"
-                    rospy.loginfo(logMessage.data)
-                    #log wheel settings
-                    rotOut = Float32()
-                    rotOut.data = self.rotation
-                    rospy.loginfo(rotOut)
-                    rospy.loginfo(self.moving)
-                    rospy.loginfo(self.settings)
+        #continue endlessly
+        while not rospy.is_shutdown():
+            logMessage = String()
+            #begin message
+            logMessage.data = "\n________________________________________"
+            logMessage.data += "\n\n\nINITIATE MESSAGE:\n\n\n"
+            rospy.loginfo(logMessage.data)
+            #log wheel settings
+            rotOut = Float32()
+            rotOut.data = self.rotation
+            rospy.loginfo(rotOut)
+            rospy.loginfo(self.moving)
+            rospy.loginfo(self.settings)
 
-                    #publish it
-                    #tests:
-                    self.pubrotation.publish(rotOut)
-                    #non-tests:
-                    self.pubwheels.publish(self.settings)
-                    self.pubmovement.publish(self.moving)
-                    #10 Hz rate regardless of joystick rate
-                    r.sleep()
+            #publish it
+            #tests:
+            self.pubrotation.publish(rotOut)
+            #non-tests:
+            self.pubwheels.publish(self.settings)
+            self.pubmovement.publish(self.moving)
+            #10 Hz rate regardless of joystick rate
+            r.sleep()
 
 if __name__ == '__main__':
 	print "Initializing Node"
