50ca65eb26123f4c5b51f5311ca28a1d7f0d651b
Amakri1020 <alexander.makrigiorgos@mail.mcgill.ca>
2015-01-28 15:58:09 -0500

    converted to a class, created addpoint methods

--- catkin_ws/src/hci/scripts/Minimap.py
+++ catkin_ws/src/hci/scripts/Minimap.py
@@ -1,93 +1,58 @@
 # -*- coding: utf-8 -*-
-"""
-Example demonstrating a variety of scatter plot features.
-"""
-
-
 from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 import numpy as np
-import Tkinter as tk
 
+class Minimap(object):
 
-class Keypress:
     def __init__(self):
-        self.root = tk.Tk()
-        self.root.geometry('300x200')
-        self.root.bind('<KeyPress>', self.onKeyPress)
 
-    def onKeyPress(self, event):
-        self.key = event.char
+        self.app = QtGui.QApplication([])
+        self.mw = QtGui.QMainWindow()
+        self.mw.resize(400,400)
+        self.view = pg.GraphicsLayoutWidget()  ## GraphicsView with GraphicsLayout inserted by default
+        self.mw.setCentralWidget(self.view)
+        self.mw.show()
+        self.mw.setWindowTitle('Minimap')
 
-    def __eq__(self, other):
-        return self.key == other
-
-    def __str__(self):
-        return self.key
-
-app = QtGui.QApplication([])
-mw = QtGui.QMainWindow()
-mw.resize(400,400)
-view = pg.GraphicsLayoutWidget()  ## GraphicsView with GraphicsLayout inserted by default
-mw.setCentralWidget(view)
-mw.show()
-mw.setWindowTitle('Minimap')
-
-## create four areas to add plots
-w2 = view.addViewBox()
-w2.setAspectLocked(False)
-w2.enableAutoRange('xy',True)
-view.nextRow()
-print("Generating data, this takes a few seconds...")
-
-## Make all plots clickable
-lastClicked = []
-def clicked(plot, points):
-    global lastClicked
-    for p in lastClicked:
-        p.resetPen()
-    print("clicked points", points)
-    for p in points:
-        p.setPen('b', width=2)
-    lastClicked = points
+        self.w2 = self.view.addViewBox()
+        self.w2.setAspectLocked(False)
+        self.w2.enableAutoRange('xy',True)
+        self.view.nextRow()
 
 
+        self.s2 = pg.ScatterPlotItem(size=10, pen=pg.mkPen('w'), pxMode=True)
+        self.x = []
+        self.y = []
+        self.waypoints_x = [0]
+        self.waypoints_y = [0]
+        self.s2.addPoints(self.waypoints_x,self.waypoints_y,size=30,symbol='t',brush='b')
+        self.w2.addItem(self.s2)
 
-## 2) Spots are transform-invariant, but not identical (top-right plot). 
-## In this case, drawing is as fast as 1), but there is more startup overhead
-## and memory usage since each spot generates its own pre-rendered image.
-
-s2 = pg.ScatterPlotItem(size=10, pen=pg.mkPen('w'), pxMode=True)
-#pos = np.random.normal(size=(2,n), scale=1e-5)
-x = []
-y = []
-waypoints_x = [0]
-waypoints_y = [0]
-#spots = [{'pos': pos[:,i], 'data': 1, 'brush':pg.intColor(i, n), 'symbol': i%5, 'size': 5+i/10.} for i in range(n)]
-#s2.addPoints(spots)
-s2.addPoints(waypoints_x,waypoints_y,size=30,symbol='t',brush='b')
-w2.addItem(s2)
-s2.sigClicked.connect(clicked)
-
-while(1):
-    s = raw_input('Enter: x,y, or waypoint: ')
-    if(s == 'waypoint'):
-        waypoints_x.append(x[-1])
-        waypoints_y.append(y[-1])
-        print "waypoint at %s,%s" %(x[-1],y[-1])
-        #s2.setSize(30,waypoints_x)
-        #s2.setSize(30,waypoints_y)
-        s2.addPoints(waypoints_x,waypoints_y,size=30,symbol='t',brush='b')
-    else:
-        a,b = [float(i) for i in s.split(',')]
-        x.append(a)
-        y.append(b)
-        s2.addPoints(x,y,size=10,symbol='o',brush='r')
-    w2.autoRange()
+        while(1):
+            s = raw_input('Enter: x,y, or waypoint: ')
+            if(s == 'waypoint'):
+                print "waypoint at %s,%s" %(self.x[-1],self.y[-1])
+                self.addWaypoint(self.x[-1],self.y[-1])
+            else:
+                try:
+                    a,b = [float(i) for i in s.split(',')]
+                    self.addPoint(a,b)
+                except ValueError:
+                    print "please enter either 'waypoint' or a valid x,y coordinate"
+            self.w2.autoRange()
+            
+    def addPoint(self,x_coord,y_coord):
+        self.x.append(x_coord)
+        self.y.append(y_coord)
+        self.s2.addPoints(self.x,self.y,size=10,symbol='o',brush='r')
+        
+    def addWaypoint(self,x_coord,y_coord):
+        self.waypoints_x.append(x_coord)
+        self.waypoints_y.append(y_coord)
+        self.s2.addPoints(self.waypoints_x,self.waypoints_y,size=30,symbol='t',brush='b')
 
 
 ## Start Qt event loop unless running in interactive mode.
 if __name__ == '__main__':
-    import sys
-    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtGui.QApplication.instance().exec_()
+    minimap = Minimap()
