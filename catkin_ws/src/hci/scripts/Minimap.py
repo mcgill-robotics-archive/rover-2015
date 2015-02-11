ddb40126f782d74c82137fcce8965356384e069e
Amakri1020 <alexander.makrigiorgos@mail.mcgill.ca>
2015-02-11 10:55:21 -0500

    updates with random pose vals from simulated data, has trouble when array gets large

--- catkin_ws/src/hci/scripts/Minimap.py
+++ catkin_ws/src/hci/scripts/Minimap.py
@@ -2,6 +2,10 @@
 from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 import numpy as np
+import rospy
+import sys
+from geometry_msgs.msg import Pose
+
 
 class Minimap(object):
 
@@ -14,6 +18,10 @@ class Minimap(object):
         self.mw.setCentralWidget(self.view)
         self.mw.show()
         self.mw.setWindowTitle('Minimap')
+        self.x = []*1000
+        self.y = []*1000
+        rospy.init_node('simulated_pose_listener', anonymous=True)
+        rospy.Subscriber('pose', Pose, self.handlePose)
 
         self.w2 = self.view.addViewBox()
         self.w2.setAspectLocked(False)
@@ -22,36 +30,40 @@ class Minimap(object):
 
 
         self.s2 = pg.ScatterPlotItem(size=10, pen=pg.mkPen('w'), pxMode=True)
-        self.x = []
-        self.y = []
         self.waypoints_x = [0]
         self.waypoints_y = [0]
         self.s2.addPoints(self.waypoints_x,self.waypoints_y,size=30,symbol='t',brush='b')
         self.w2.addItem(self.s2)
 
-        while(1):
-            s = raw_input('Enter: x,y, or waypoint: ')
-            if(s == 'waypoint'):
-                print "waypoint at %s,%s" %(self.x[-1],self.y[-1])
-                self.addWaypoint(self.x[-1],self.y[-1])
-            else:
-                try:
-                    a,b = [float(i) for i in s.split(',')]
-                    self.addPoint(a,b)
-                except ValueError:
-                    print "please enter either 'waypoint' or a valid x,y coordinate"
-            self.w2.autoRange()
+#        while(1):
+#            s = raw_input('Enter: x,y, or waypoint: ')
+#            if(s == 'waypoint'):
+#                print "waypoint at %s,%s" %(self.x[-1],self.y[-1])
+#                self.addWaypoint(self.x[-1],self.y[-1])
+#            else:
+#                try:
+#                    a,b = [float(i) for i in s.split(',')]
+#                    self.addPoint(a,b)
+#                except ValueError:
+#                    print "please enter either 'waypoint' or a valid x,y coordinate"
+            
+        sys.exit(self.app.exec_())
             
     def addPoint(self,x_coord,y_coord):
         self.x.append(x_coord)
         self.y.append(y_coord)
-        self.s2.addPoints(self.x,self.y,size=10,symbol='o',brush='r')
+        self.s2.setData(self.x,self.y,size=10,symbol='o',brush='r')
+        self.s2.addPoints(self.waypoints_x,self.waypoints_y,size=30,symbol='t',brush='b')
+        self.w2.autoRange()
         
     def addWaypoint(self,x_coord,y_coord):
         self.waypoints_x.append(x_coord)
         self.waypoints_y.append(y_coord)
         self.s2.addPoints(self.waypoints_x,self.waypoints_y,size=30,symbol='t',brush='b')
-
+        self.w2.autoRange()
+        
+    def handlePose(self, data):
+        self.addPoint(data.position.x,data.position.y)
 
 ## Start Qt event loop unless running in interactive mode.
 if __name__ == '__main__':
