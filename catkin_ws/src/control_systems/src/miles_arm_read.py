d3a52a813f3d06f5c50cc7c53b03f671b50ed74b
mdtmc <miles.cranmer@gmail.com>
2015-03-03 21:52:09 -0500

    Added ros capabilities to new arm code, untested

--- catkin_ws/src/control_systems/src/miles_arm_read.py
+++ catkin_ws/src/control_systems/src/miles_arm_read.py
@@ -1,14 +1,83 @@
+#!/usr/bin/python
+
 from math import sqrt, atan, pi
+import rospy
+from control_systems.msg import ArmMotion, ArmAngles
+from std_msgs.msg import String
 
 #a1 is the length of the upper arm (touches base)
-a1 = 1
+a1 = rospy.get_param('control/ln_upperarm',1)
 #a2 is the length of the forearm (attached to hand)
-a2 = 1
+a2 = rospy.get_param('control/ln_forearm',1)
 
 forearmLowerBound = pi/6
 forearmUpperBound = 5*pi/4
 upperarmLowerBound = -pi/2
 upperarmUpperBound = pi/2
+
+
+class ArmControlReader(object):
+    def __init__ (self):
+        #initiate node
+        rospy.init_node('arm_reader')
+        #publish arm settings to this topic
+        self.pubArm = rospy.Publisher('/arm',ArmAngles,queue_size=10,latch=10)
+        #settings to be read in
+        self.settings = ArmMotion()
+        #set values to off
+        self.settings.x = 0
+        self.settings.y = 0
+        self.settings.theta = 0
+        self.settings.on = False
+        #angles:
+        #angle at base
+        self.angles = ArmAngles()
+        # set all angles to zero
+        self.angles.shoulderOrientation = 0
+        self.angles.shoulderElevation = 0
+        self.angles.elbow = 0
+        self.angles.wristOrientation = 0
+        self.angles.wristElevation = 0
+
+        rospy.Subscriber('/cmd_arm', ArmMotion, self.update_settings,
+            queue_size=10)
+
+    def update_settings(self,msg):
+        #import readings into object
+        self.settings.x = msg.x
+        self.settings.y = msg.y
+        #ADJUST BOUNDS HERE!!!
+        self.settings.on = msg.on
+
+        #calculate new angles for robotic arm
+        #these are the angles for movement in the x-y plane
+        newSetting = nextAngle(
+            (self.angles.shoulderElevation,self.angles.elbow),
+            self.settings.x,self.settings.y)
+
+        #if new angle is good, update
+        if newSetting[1]:
+        	(self.angles.shoulderElevation,
+        		self.angles.elbow) = newSetting[0]
+
+        #this is the angle of the x-y plane relative to the forward direction
+        #of the robot
+        newOrientation = msg.theta
+        if newOrientation <= pi/2 and newOrientation >= -pi/2:
+        	self.angles.shoulderOrientation = self.settings.theta
+
+    #function will publish at 60Hz
+    def run(self):
+        r = rospy.Rate(60)
+        #continue until quit
+        while not rospy.is_shutdown():
+            #publish to topic
+            self.pubArm.publish(self.angles)
+            #next iteration
+            r.sleep()
+
+
+
 
 def sgn(x):
 	if x == 0:
@@ -100,6 +169,13 @@ def possibleAngles (x, y):
 #will return angles, and boolean to say
 #whether arm should stop or not
 def nextAngle(initialAngles, x, y, psi):
+	#quick check for safety
+	if a1+a2 < distance(x,y):
+		#point is unreachable;
+		#don't calculate anything!
+		return [[0,0],False]
+
+
 	#get possible angles to move to
 	(angleset1,angleset2) = possibleAngles(x,y)
 	#both sets are not good until tested
