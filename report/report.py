4ac1e2ca1ce937bb8c8ef093438128ab7d00e7e4
mdtmc <miles.cranmer@gmail.com>
2015-03-14 01:37:04 -0400

    Adding ros capabilities to excel report generator

--- report/report.py
+++ report/report.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
-import xlsxwriter
-import rospy
+import xlsxwriter,rospy,time
 from control_systems.msg import SetPoints
 from std_msgs.msg import Int16
+from odometry.msg import RoverSpeed
 
-batteryColumn = 1
+1 = 10
+
 
 
 class GenerateReport():
@@ -13,26 +14,42 @@ class GenerateReport():
         filename = "report.xlsx"
         self.document = xlsxwriter.Workbook("report.xlsx")        
         self.graphSheet = self.document.add_worksheet('graphs')
-        self.dataSheet = self.document.add_worksheet('data')   
+        self.dataSheet = self.document.add_worksheet('data')
+        self.dataSheetTimed = self.document.add_worksheet('timedData')
+        self.speeds = RoverSpeed()
       
         self.rowIndex = 0
+        self.timeIndex = 0
 
         battery1topic = rospy.get_param("battery1topic", "integers")
         rospy.init_node("excelBuilder", anonymous=False)
 
         self.sub1 = rospy.Subscriber(battery1topic, Int16, self.recordBattery)
-        rospy.spin()
+        
+        rospy.Subscriber('/odo_speeds',RoverSpeed,self.update_speeds,
+            queue_size=10)
 
+    def update_speeds(self, msg):
+        self.speeds = msg
+
+    def recordSpeeds(self):
+        self.dataSheetTimed.write(self.timeIndex, 1, time.time())
+        self.dataSheetTimed.write(self.timeIndex, 2, self.speeds.linear)
+
+        self.timeIndex += 1
 
     def recordBattery(self, batteryLevel):
-        self.dataSheet.write(self.rowIndex, batteryColumn, batteryLevel.data)
-        self.dataSheet.write(self.rowIndex, batteryColumn+1, batteryLevel.data-1)
+        self.dataSheet.write(self.rowIndex, 1, batteryLevel.data)
+        self.dataSheet.write(self.rowIndex, 2, 
+            batteryLevel.data-1)
         self.rowIndex += 1
 
 
     def createGraph(self): 
-        batterySerie1 = ['data', 0, batteryColumn, self.rowIndex-1, batteryColumn]  
-        batterySerie2 = ['data', 0, batteryColumn+1, self.rowIndex-1, batteryColumn+1]  
+        batterySerie1 = ['data', 0, 1, self.rowIndex-1, 
+            1]  
+        batterySerie2 = ['data', 0, 2, self.rowIndex-1, 
+            2]  
         gr1 = self.document.add_chart({'type': 'line'})
         gr1.add_series({
             'name':     'Battery 1',
@@ -51,6 +68,17 @@ class GenerateReport():
         # Insert the chart into the dataSheet (with an offset).
         self.graphSheet.insert_chart('A1', gr1)
 
+    def run(self):
+        #calculate required wheel angles, speeds
+        r = rospy.Rate(60)
+        #continue endlessly
+        while not rospy.is_shutdown():
+            #update time
+            self.clock = time.time()
+            #write the current speeds to file
+            self.recordSpeeds()
+            r.sleep()
+        self.close()
 
     def close(self):
         self.createGraph()
@@ -58,9 +86,9 @@ class GenerateReport():
         print "exit"
 
 if __name__=="__main__":
-    reportGernerate=None
-    try:
-        reportGernerate=GenerateReport()
-    finally:
-        reportGernerate.close()
-        print "Clean exit"
+    print "Init node"
+    graphGen=GenerateReport()
+    print "Running node"
+    graphGen.run()
+    rospy.spin()
+    print "Clean --------  ~     "
