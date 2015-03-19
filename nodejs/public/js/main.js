19c2d65dc1cf44135051e7179a94b1d3e7318013
hyz <zhao_hanyang@hotmail.com>
2015-03-19 01:26:22 -0400

    Started making the website

---
+++ nodejs/public/js/main.js
@@ -0,0 +1,38 @@
+
+    function loadpose(){
+        var socket = io();
+        var posearray = []
+        socket.on('sendPose', function(msg){
+            console.log(msg);
+            posearray[0]=msg[0];
+            posearray[1]=msg[1];
+            posearray[2]=msg[2];
+            posearray[3]=msg[2];
+            document.getElementById("sendPose1").innerHTML = posearray[0];
+            document.getElementById("sendPose2").innerHTML = posearray[1];
+            document.getElementById("sendPose3").innerHTML = posearray[2];
+            document.getElementById("sendPose4").innerHTML = posearray[3];
+        });
+    }
+
+    function loadtime(){
+        var socket = io();
+        socket.on('time', function(data) {
+            console.log(data);
+            $('#time').html(data.time);
+        });
+    }
+
+    function start(){
+        loadpose();
+        loadtime();
+    }
+
+
+    document.addEventListener('polymer-ready', function() {
+      var navicon = document.getElementById('navicon');
+      var drawerPanel = document.getElementById('drawerPanel');
+      navicon.addEventListener('click', function() {
+        drawerPanel.togglePanel();
+      });
+    });
