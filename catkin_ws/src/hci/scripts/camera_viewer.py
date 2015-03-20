a30e2629d92d3e41dbf00979f37597bd12ac2e09
artemis <mcgillroboticsteam@gmail.com>
2015-03-20 10:39:12 -0400

    cleanup of camera_viewer.py

--- catkin_ws/src/hci/scripts/camera_viewer.py
+++ catkin_ws/src/hci/scripts/camera_viewer.py
@@ -1,89 +1,108 @@
 #!/usr/bin/python
 
 from VideoWindow import *
-##from no_imu import *
 from PyQt4 import QtCore, QtGui
-from sensor_msgs.msg import Image
-##import publisher
 
 import sys
 import signal
-import rospy
 
+import rospy
 from sensor_msgs.msg import Image
 
 class CentralUi(QtGui.QMainWindow):
 
     def __init__(self, parent=None):
         super(CentralUi,self).__init__(parent)
-       
-        self.image1=None
-        self.image2=None
-        self.image3=None
-      
+        
+        self.feedTopics=[]
+        self.imageMain = None
+        self.imageTop = None
+        self.imageBottom = None     
+     
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        
         self.signal=QtCore.QTimer(self)
         self.signal.timeout.connect(self.repaint_image)
-        self.listener()
+        
+        self.ros_init()
+
         self.signal.start(100)
     
-    def callback3(self, data):
+ 
+ 
+    def receiveimageMain(self, data):
         try:
-        	self.image3 = data
-        finally:
-        	pass
-    def callback2(self, data):
-        try:
-            self.image2=data
+            self.imageMain = data
         finally:
             pass
-    def callback1(self, data):
+
+
+    def receiveimageTop(self, data):
         try:
-            self.image1=data
+            self.imageTop = data
         finally:
             pass
+
+
+    def receiveimageBottom(self, data):
+        try:
+            self.imageBottom = data
+        finally:
+            pass
+   
 
     def repaint_image(self):
-        if self.image2 is not None:
+        if self.imageMain is not None:
             try:
-                qimage2 = QtGui.QImage(self.image2.data, self.image2.width/3, self.image2.height/3,QtGui.QImage.Format_RGB888)
+                qimageMain = QtGui.QImage(self.imageMain.data, self.imageMain.width/3, self.imageMain.height/3,QtGui.QImage.Format_RGB888)
                 #TODO: FIGURE OUT THIS DIVIDED BY 3 !!! 
                 # wiskey-tango-foxtrot
-                image2 = QtGui.QPixmap.fromImage(qimage2)     
+                imageMain = QtGui.QPixmap.fromImage(qimageMain)     
             finally:
                 pass
-            self.ui.camera2.setPixmap(image2)
-        else:
-            self.ui.camera2.setText("no video feed")
-        if self.image3 is not None:
-        	try: 
-        		qimage3 = QtGui.QImage(self.image3.data, self.image3.width/3, self.image3.height/3,QtGui.QImage.Format_RGB888)
-        		#TODO: FIGURE OUT THIS DIVIDED BY 3 !!! 
-        		# wiskey-tango-foxtrot
-        		image3 = QtGui.QPixmap.fromImage(qimage3)		
-        	finally:
-        		pass
-        	self.ui.camera3.setPixmap(image3)
-        else:
-        	self.ui.camera3.setText("no video feed")
-        if self.image1 is not None:
-            try: 
-                qimage1 = QtGui.QImage(self.image1.data, self.image1.width/3, self.image1.height/3,QtGui.QImage.Format_RGB888)
-                #TODO: FIGURE OUT THIS DIVIDED BY 3 !!! 
-                # wiskey-tango-foxtro
-                image1 = QtGui.QPixmap.fromImage(qimage1)     
-            finally:
-                pass
-            self.ui.camera1.setPixmap(image1)
+            self.ui.camera1.setPixmap(imageMain)
         else:
             self.ui.camera1.setText("no video feed")
+        
+        if self.imageTop is not None:
+        	try: 
+        		qimageTop = QtGui.QImage(self.imageTop.data, self.imageTop.width/3, self.imageTop.height/3,QtGui.QImage.Format_RGB888)
+        		#TODO: FIGURE OUT THIS DIVIDED BY 3 !!! 
+        		# wiskey-tango-foxtrot
+        		imageTop = QtGui.QPixmap.fromImage(qimageTop)		
+        	finally:
+        		pass
+        	self.ui.camera2.setPixmap(imageTop)
+        else:
+        	self.ui.camera2.setText("no video feed")
 
-    def listener(self):
-        rospy.init_node('listener',anonymous=False)
-        self.listen3=rospy.Subscriber("/camera_front_right/camera/image_raw",Image, self.callback3)
-        self.listen2=rospy.Subscriber("/camera_front_right/camera/image_raw",Image, self.callback2)
-        self.listen1=rospy.Subscriber("/camera_front_right/camera/image_raw",Image, self.callback1)
+        if self.imageBottom is not None:
+            try: 
+                qimageBottom = QtGui.QImage(self.imageBottom.data, self.imageBottom.width/3, self.imageBottom.height/3,QtGui.QImage.Format_RGB888)
+                #TODO: FIGURE OUT THIS DIVIDED BY 3 !!! 
+                # wiskey-tango-foxtro
+                imageBottom = QtGui.QPixmap.fromImage(qimageBottom)     
+            finally:
+                pass
+            self.ui.camera3.setPixmap(imageBottom)
+        else:
+            self.ui.camera3.setText("no video feed")
+
+
+    def ros_init(self):
+        rospy.init_node('camera_viewer',anonymous=True)  
+        self.getimageTopic() 
+        rospy.Subscriber(self.feedTopics[0], Image, self.receiveimageMain)
+        rospy.Subscriber(self.feedTopics[1], Image, self.receiveimageTop)
+        rospy.Subscriber(self.feedTopics[2], Image, self.receiveimageBottom)
+
+
+    def getimageTopic(self):
+        self.feedTopics.append(rospy.get_param("feed/topicMain", "/feed1/image_raw"))
+        self.feedTopics.append(rospy.get_param("feed/topicTop", "/feed2/image_raw"))
+        self.feedTopics.append(rospy.get_param("feed/topicBottom", "/feed3/image_raw"))
+        rospy.loginfo(self.feedTopics)
 
 
 if __name__ == "__main__":
