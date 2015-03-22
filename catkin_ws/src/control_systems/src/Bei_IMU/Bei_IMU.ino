53ec632e4e267348ddcdc177f2a729ae8e92c4e3
mdtmc <miles.cranmer@gmail.com>
2015-03-21 21:22:39 -0400

    Starting total calibration on Bei's IMU. Offsets initialized

--- catkin_ws/src/control_systems/src/Bei_IMU/Bei_IMU.ino
+++ catkin_ws/src/control_systems/src/Bei_IMU/Bei_IMU.ino
@@ -2,8 +2,16 @@
 #include <Wire.h>
 
 const int mpu = 0x68; //I2C address of the MPU-6050
-//int16_t
 long double ax, ay, az, gx, gy, gz, tmp;
+//offsets
+double axo = 0;
+double ayo = 0;
+double azo = 0;
+double tmpo = 0;
+double gxo = 0;
+double gyo = 0;
+double gzo = 0;
+
 long double sumforavg = 0;
 int measures = 0;
 void setup()
@@ -22,6 +30,8 @@ void loop()
   Wire.write(0x3B);
   Wire.endTransmission(false);
   Wire.requestFrom(mpu,14,true);
+  
+  
   ax=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
   ay=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   az=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
@@ -29,10 +39,18 @@ void loop()
   gx=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   gy=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   gz=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  //factor in offsets
+  ax  += axo;
+  ay  += ayo;
+  az  += azo;
+  tmp += tmpo;
+  gx  += gxo;
+  gy  += gyo;
+  gz  += gzo;
   
-  //Serial.print("ax = "); Serial.println(ax);
-  //Serial.print("ay = "); Serial.println(ay);
-  //Serial.print("az = "); Serial.println(az);
+  
+  //magnitude of acceleration
   long double accel = 9.81 * sqrt(ax*ax + ay*ay + az*az)/16269.73;
   //sumforavg += accel;
   //measures ++;
