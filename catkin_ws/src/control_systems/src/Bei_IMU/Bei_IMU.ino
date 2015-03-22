4bc2b7f9bf5252cbfc35a630bec1f32425aa9fd5
mdtmc <miles.cranmer@gmail.com>
2015-03-21 23:57:09 -0400

    instead doing the calibration from computer

--- catkin_ws/src/control_systems/src/Bei_IMU/Bei_IMU.ino
+++ catkin_ws/src/control_systems/src/Bei_IMU/Bei_IMU.ino
@@ -1,16 +1,13 @@
+//current problems 
+//the accelerometer is connected to the gyroscope, which is 
+//not calibrated. When I calibrate the gyro I must also adjust
+//the vector for the acceleration
+
 //pins for SDA, SCL already declared as variables
 #include <Wire.h>
 
 const int mpu = 0x68; //I2C address of the MPU-6050
 long double ax, ay, az, gx, gy, gz, tmp;
-//offsets
-double axo = 0;
-double ayo = 0;
-double azo = 0;
-double tmpo = 0;
-double gxo = 0;
-double gyo = 0;
-double gzo = 0;
 
 long double sumforavg = 0;
 int measures = 0;
@@ -32,23 +29,13 @@ void loop()
   Wire.requestFrom(mpu,14,true);
   
   
-  ax=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-  ay=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  az=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  ax= Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  ay= Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  az= Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
   tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  gx=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  gy=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  gz=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-
-  //factor in offsets
-  ax  += axo;
-  ay  += ayo;
-  az  += azo;
-  tmp += tmpo;
-  gx  += gxo;
-  gy  += gyo;
-  gz  += gzo;
-  
+  gx= Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  gy= Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  gz= Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
   
   //magnitude of acceleration
   long double accel = 9.81 * sqrt(ax*ax + ay*ay + az*az)/16269.73;
@@ -58,7 +45,13 @@ void loop()
   //Serial.print("accel = "); Serial.println(accel);
   //Serial.print("avgaccel = "); Serial.print(average);
   //Serial.print(", curr =  ");Serial.println((double)accel);
-  Serial.println((double)accel);
+  //Serial.println((double)accel);
+  Serial.print((double)gx);
+  Serial.print(",");Serial.print((double)gy);
+  Serial.print(",");Serial.print((double)gz);
+  Serial.print(",");Serial.print((double)ax);
+  Serial.print(",");Serial.print((double)ay);
+  Serial.print(",");Serial.println((double)az);
   
   delay(50);
 }
