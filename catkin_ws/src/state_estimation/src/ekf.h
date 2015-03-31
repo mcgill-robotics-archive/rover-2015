ae21988e264f8f59804ffb03c9e9d0d205376cba
jmacneal <jake.macneal@gmail.com>
2015-03-31 17:15:45 -0400

    added filler code for functions to ekf.cpp

--- catkin_ws/src/state_estimation/src/ekf.h
+++ catkin_ws/src/state_estimation/src/ekf.h
@@ -5,10 +5,13 @@
 #ifndef EKF
 #define EKF
 
+#include "include/Eigen/Dense"
 #include <Eigen/Dense>
 using namespace Eigen;
 
-const int SENSOR_DIMS = 6;
+namespace ekf{
+
+const int SENSOR_DIMS = 3;
 const int STATE_DIMS = 6;
 
 typedef Matrix<double, SENSOR_DIMS, SENSOR_DIMS> SquareSensorMatrix;
@@ -18,31 +21,31 @@ typedef Matrix<double, STATE_DIMS, 1> StateVector;
 typedef Matrix<double, SENSOR_DIMS, STATE_DIMS> StateToSensorMatrix;
 typedef Matrix<double, STATE_DIMS, SENSOR_DIMS> SensorToStateMatrix;
 
-class ekf
+class EKF
 {
 public:
-	inline ekf(SensorVector z, const SquareStateMatrix P, const SquareStateMatrix Q, const SquareStateMatrix R){
-		this.z = z;												   // Point the z measurement matrix to a supplied measurement matrix
-		this.P = new SquareStateMatrix(P);						   // Copy-by-value the input initial P, Q, and R matrices into the EKF
-		this.Q = new SquareStateMatrix(Q);
-		this.R = new SquareSensorMatrix(R);
+	EKF(SensorVector *z, const SquareStateMatrix &P, const SquareStateMatrix &Q, const SquareStateMatrix &R){
+		this->z = z; // Copy-by-value the input initial P, Q, and R matrices into the EKF
+		this.P = P;
+		this.Q = Q;
+		this.R = R;
 	};
 
 	/*
 	  Prediction step, X_k|k-1 and P_k|k-1 are predicted based on previous updated values.
 	 */	  
-	inline void predict(){
+	void predict(){
 		F = FCalc();
-		X = XPredict();
-		P = PPredict():
+		X = XPredict(X);
+		P = PPredict(P, F):
 	};
 
 	/*
 	  Update step, X_k|k, y_k, K_k, P_k|k are all updated based on obsevation.
 	 */
-	inline void update(){
+	void update(){
 		H = HCalc();
-		y = yUpdate();
+		y = yUpdate(X);
 		K = KUpdate();
 		X = XUpdate();
 		P = PUpdate();
@@ -53,19 +56,19 @@ private:
 	/*
 	  Prediction-step functions
 	 */
-	SquareStateMatrix FCalc();									   // Calculate the updated Covariance Transition Matrix (Jacobian)
-	StateVector XPredict();										   // Predict the next State based on f (transition) and previous X
-	SquareStateMatrix PPredict();								   // Predict the next Covariance Matrix based on F and previous P
+	SquareStateMatrix FCalc(); // Calculate the updated Covariance Transition Matrix (Jacobian)
+	StateVector XPredict(StateVector &previous_X);	// Predict the next State based on f (transition) and previous X
+	SquareStateMatrix PPredict(SquareStateMatrix &previous_P, SquareStateMatrix &F); // Predict the next Covariance Matrix based on F and previous P
 
 
 	/*
 	  Update-step functions
 	 */
-	StateToSensorMatrix HCalc();								   // Calculate the Measurement Transformation Matrix (Jacobian)
-	SensorVector yUpdate();										   // Update y based on current z (measurement) and X (state)
-	SensorToStateMatrix KUpdate();								   // Update the Kalman Gain, based on the estimated P, H and R
-	StateVector XUpdate();										   // Update the State estimate based on K and y
-	SquareStateMatrix PUpdate();								   // Update the Error Covariance Estimate given K and H
+	StateToSensorMatrix HCalc(); // Calculate the Measurement Transformation Matrix (Jacobian)
+	SensorVector yUpdate(StateVector &X);	// Update y based on current z (measurement) and X (state)
+	SensorToStateMatrix KUpdate(); // Update the Kalman Gain, based on the estimated P, H and R
+	StateVector XUpdate(); // Update the State estimate based on K and y
+	SquareStateMatrix PUpdate(); // Update the Error Covariance Estimate given K and H
 	
 
 	/*
@@ -82,7 +85,7 @@ private:
 	  Observation variables
 	*/
 	SensorVector y;
-	SensorVector z;
+	SensorVector *z;
 	const StateToSensorMatrix h;
 
 	/*
@@ -96,7 +99,7 @@ private:
 	*/
 	const SquareStateMatrix Q;
 	const SquareSensorMatrix R;	
+};
+
 }
-
-
 #endif
