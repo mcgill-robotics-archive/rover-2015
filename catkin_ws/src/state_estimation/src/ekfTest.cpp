b25f06be16339f03218a978d4fb20f819b7ff9b4
marcyang001 <m.y.yang001@gmail.com>
2015-03-31 18:43:35 -0400

    newEKF.h makefile

--- catkin_ws/src/state_estimation/src/ekfTest.cpp
+++ catkin_ws/src/state_estimation/src/ekfTest.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include <Eigen/Dense>
+//#include <Eigen/Dense>
 #include "newEKF.h"
 //#include "ekf.h"
-
+using namespace std;
 using Eigen::MatrixXd;
 
 //typedef Matrix<double, SENSOR_DIMS, SENSOR_DIMS> SquareSensorMatrix;
@@ -12,18 +12,24 @@ int main()
   	int i, j;
   	SquareSensorMatrix matrix1;
   	SquareStateMatrix matrix2;
-	//SensorVector z;
-	StateVector matrix4;
-	StateToSensorMatrix matrix5;
-	SensorToStateMatrix matrix6;
+	SensorVector *z;
   	//Matrix<double, 100,100> matrix2;
 
-	for (i = 0; i < 6; i++) {
-		for (j = 0; j < 6; j++) {
-			matrix1(i,j) = 1;
-		}
-	}
+	matrix1(0, 0) = 123;
+
+	matrix2(0, 0) = 12312412;
+
+	SensorVector y; 
+
+	y(0, 0) = 45678910;
 	
+	z = &y;
+
+	EKF ekf(&y, matrix1, matrix2, matrix2);
+
+
+
+
 
 
 	MatrixXd m(3,3);
@@ -31,7 +37,11 @@ int main()
 	m(1,0) = 2.5;
 	m(0,1) = -1;
 	m(1,1) = m(1,0) + m(0,1);
-	std::cout << matrix1 << std::endl;
+	cout << *z << std::endl;
+	//cout << *ekf.z << endl;
+	y(1, 0) = 222;
+	//z = &y;
+	cout << *ekf.z << endl;
 
 	return 0;
 }
--- catkin_ws/src/state_estimation/src/newEKF.h
+++ catkin_ws/src/state_estimation/src/newEKF.h
@@ -1,5 +1,5 @@
-#ifndef EKF
-#define EKF
+#ifndef EKF_HEADER
+#define EKF_HEADER
 
 #include <Eigen/Dense>
 using namespace Eigen;
@@ -14,20 +14,60 @@ typedef Matrix<double, STATE_DIMS, 1> StateVector;
 typedef Matrix<double, SENSOR_DIMS, STATE_DIMS> StateToSensorMatrix;
 typedef Matrix<double, STATE_DIMS, SENSOR_DIMS> SensorToStateMatrix;
 
-class ekf
+class EKF
 {
 public:
-	inline ekf(SensorVector z, const SquareStateMatrix P, const SquareStateMatrix Q, const SquareSensorMatrix R){
-		this->z = z;												   // Point the z measurement matrix to a supplied measurement matrix
-	 	this->P = P;						   // Copy-by-value the input initial P, Q, and R matrices into the EKF
+	EKF(SensorVector *z, const SquareStateMatrix P, const SquareStateMatrix Q, const SquareStateMatrix R){
+		this->z = z; // Copy-by-value the input initial P, Q, and R matrices into the EKF
+		this->P = P;
 		this->Q = Q;
 		this->R = R;
 	};
 
-		SensorVector z;
-		SquareStateMatrix P;
-		SquareStateMatrix Q;
-		SquareSensorMatrix R;
+	SensorVector *z;
+	SquareStateMatrix P;
+	SquareStateMatrix Q;
+	SquareSensorMatrix R;
+
+	void predict(){
+		F = FCalc();
+		X = XPredict(X);
+		P = PPredict(P, F);
+	};
+
+	void update(){
+		H = HCalc();
+		y = yUpdate(X);
+		K = KUpdate();
+		X = XUpdate();
+		P = PUpdate();
+	};
+
+
+
+private:
+	const SquareStateMatrix f;
+	SquareStateMatrix F;
+	StateVector X;
+	SensorToStateMatrix K;	
+
+	SensorVector y;
+	const StateToSensorMatrix h;
+	StateToSensorMatrix H;	
+
+
+	SquareStateMatrix FCalc(); // Calculate the updated Covariance Transition Matrix (Jacobian)
+	StateVector XPredict(StateVector &previous_X);	// Predict the next State based on f (transition) and previous X
+	SquareStateMatrix PPredict(SquareStateMatrix &previous_P, SquareStateMatrix &F); // Predict the next Covariance Matrix based on F and previous P
+
+	StateToSensorMatrix HCalc(); // Calculate the Measurement Transformation Matrix (Jacobian)
+	SensorVector yUpdate(StateVector &X);	// Update y based on current z (measurement) and X (state)
+	SensorToStateMatrix KUpdate(); // Update the Kalman Gain, based on the estimated P, H and R
+	StateVector XUpdate(); // Update the State estimate based on K and y
+	SquareStateMatrix PUpdate(); // Update the Error Covariance Estimate given K and H
+
+
+
 };
 
 
