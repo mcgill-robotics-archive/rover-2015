d74de62a6a199ce38b45a0523729a77486b51d0e
Zeyad Saleh <zeyadhazemsaleh@gmail.com>
2015-04-15 14:22:25 -0400

    Removed Command, sending argument as a whole

--- Arduino/RS485/Rs485Test.c
+++ Arduino/RS485/Rs485Test.c
@@ -5,30 +5,27 @@ typedef unsigned int uint;
 int main () {
 
     uint address = 102;
-    uint command = 0;
-    uint function = 127;
-    uint argumentLo = 200;
-    uint argumentMid1 = 20;
+    uint function = 200;
+    uint argumentLo = 20;
+    uint argumentMid1 = 150;
     uint argumentMid2 = 99;
     uint argumentHi = 67;
+    uint argument = ((argumentLo << 24) + (argumentMid1<<16) + (argumentMid2 <<8) + argumentHi);
     uint termination = 255;
-	
+    
 
-    uint messageLo;
-    messageLo = (address << 24) + (command << 23) + (function << 16) + (argumentLo << 8) + (argumentMid1);
+    uint messageLo = (address << 24) + (function << 16) + (argument >> 16);
     uint messageHi = 0b11101111111011111111111111111111;
 
     printf("messageLo = %d \n" , messageLo);
 
     uint intArray[] = {messageLo,messageHi};
     uint getAddress = (intArray[0] & 0b11111111000000000000000000000000) >> 24;
-    uint getCommand = (intArray[0] & 0b00000000100000000000000000000000) >> 23;
-    uint getFunction = (intArray[0] & 0b00000000011111110000000000000000) >> 16;
+    uint getFunction = (intArray[0] & 0b00000000111111110000000000000000) >> 16;
     uint getArgumentLo = (intArray[0] & 0b00000000000000001111111100000000) >> 8;
     uint getArgumentMid1 = (intArray[0] & 0b00000000000000000000000011111111);
 
     printf("getAddress = %d \n" , getAddress);
-    printf("getCommand = %d \n" , getCommand);
     printf("getFunction = %d \n", getFunction);
     printf("getArgumentLo = %d \n", getArgumentLo);
     printf("getArgumentMid1 = %d \n", getArgumentMid1);
