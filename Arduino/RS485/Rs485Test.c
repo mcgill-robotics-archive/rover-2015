092b326dc49de8c6e14f2efe4485f2cdfc39b153
David Lavoie-Boutin <d.lavoie.boutin@me.com>
2015-04-15 13:59:08 -0400

    type def for unsigned int much cleanup, keep those indents

--- Arduino/RS485/Rs485Test.c
+++ Arduino/RS485/Rs485Test.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
 
-int main(void) {
+typedef unsigned int uint;
 
-int address = 102;
-int command = 0;
-int function = 127;
-int argumentLo = 200;
-int argumentMid1 = 20;
-int argumentMid2 = 99;
-int argumentHi = 67;
-int termination = 255;
+int main () {
+
+    uint address = 102;
+    uint command = 0;
+    uint function = 127;
+    uint argumentLo = 200;
+    uint argumentMid1 = 20;
+    uint argumentMid2 = 99;
+    uint argumentHi = 67;
+    uint termination = 255;
 	
-// your code goes here
-unsigned int messageLo;
-messageLo = (address << 24) + (command << 23) + (function << 16) + (argumentLo << 8) + (argumentMid1);
-unsigned int messageHi = 0b11101111111011111111111111111111;
 
-printf("messageLo = %d \n" , messageLo);
+    uint messageLo;
+    messageLo = (address << 24) + (command << 23) + (function << 16) + (argumentLo << 8) + (argumentMid1);
+    uint messageHi = 0b11101111111011111111111111111111;
 
-unsigned int intArray[] = {messageLo,messageHi};
-unsigned int getAddress = (intArray[0] & 0b11111111000000000000000000000000) >> 24;
-unsigned int getCommand = (intArray[0] & 0b00000000100000000000000000000000) >> 23;
-unsigned int getFunction = (intArray[0] & 0b00000000011111110000000000000000) >> 16;
-unsigned int getArgumentLo = (intArray[0] & 0b00000000000000001111111100000000) >> 8;
-unsigned int getArgumentMid1 = (intArray[0] & 0b00000000000000000000000011111111);
-printf("getAddress = %d \n" , getAddress);
-printf("getCommand = %d \n" , getCommand);
-printf("getFunction = %d \n", getFunction);
-printf("getArgumentLo = %d \n", getArgumentLo);
-printf("getArgumentMid1 = %d \n", getArgumentMid1);
-return 0;
+    printf("messageLo = %d \n" , messageLo);
+
+    uint intArray[] = {messageLo,messageHi};
+    uint getAddress = (intArray[0] & 0b11111111000000000000000000000000) >> 24;
+    uint getCommand = (intArray[0] & 0b00000000100000000000000000000000) >> 23;
+    uint getFunction = (intArray[0] & 0b00000000011111110000000000000000) >> 16;
+    uint getArgumentLo = (intArray[0] & 0b00000000000000001111111100000000) >> 8;
+    uint getArgumentMid1 = (intArray[0] & 0b00000000000000000000000011111111);
+
+    printf("getAddress = %d \n" , getAddress);
+    printf("getCommand = %d \n" , getCommand);
+    printf("getFunction = %d \n", getFunction);
+    printf("getArgumentLo = %d \n", getArgumentLo);
+    printf("getArgumentMid1 = %d \n", getArgumentMid1);
+
+    return 0;
 }
