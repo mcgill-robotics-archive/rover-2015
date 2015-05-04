61ae64de819e30dacd6017ae7fd15bb740e58882
David Lavoie-Boutin <d.lavoie.boutin@me.com>
2015-05-03 17:50:56 -0700

    added gps logger to log mechanism

--- catkin_ws/src/logger/src/report.py
+++ catkin_ws/src/logger/src/report.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python
 
 import xlsxwriter,rospy,time
-from control_systems.msg import SetPoints
 from std_msgs.msg import Int16
 from odometry.msg import RoverSpeed
 from logRosout import LogRosout
+from logGPS import LogRawGPS
+
 
 class GenerateReport():
     def __init__(self):
         filename = "report.xlsx"
-        self.document = xlsxwriter.Workbook("report.xlsx")        
+        self.document = xlsxwriter.Workbook(filename)
         self.graphSheet = self.document.add_worksheet('graphs')
         self.dataSheets = [self.document.add_worksheet('data'),
-            self.document.add_worksheet('timedData')]
+                           self.document.add_worksheet('timedData')]
         self.speeds = RoverSpeed()
         self.rosout_sheet = self.document.add_worksheet("rosout")
-        
-        self.logRosout=LogRosout(self.document, self.rosout_sheet)
+        self.gps_sheet = self.document.add_worksheet("gps")
 
-        
-      
-        self.index = [0,0]
+        self.logRosout = LogRosout(self.document, self.rosout_sheet)
+        self.logGPS = LogRawGPS(self.document, self.gps_sheet)
+
+        self.index = [0, 0]
         self.graphs = 0
         self.startTime = time.time()
 
@@ -29,8 +30,7 @@ class GenerateReport():
 
         self.sub1 = rospy.Subscriber(battery1topic, Int16, self.recordBattery)
         
-        rospy.Subscriber('/odo_speeds',RoverSpeed,self.update_speeds,
-            queue_size=10)
+        rospy.Subscriber('/odo_speeds', RoverSpeed, self.update_speeds, queue_size=10)
 
     def update_speeds(self, msg):
         self.speeds = msg
@@ -46,7 +46,6 @@ class GenerateReport():
         self.dataSheet.write(self.index[0], 2, 
             batteryLevel.data-1)
         self.index[0] += 1
-
 
     def createGraph(self, sheet, x, y, labelx, labely, title): 
         serie1 = ['timedData', 0, x, self.index[sheet]-1,x] 
@@ -72,13 +71,13 @@ class GenerateReport():
 
     def run(self):
         print "Enter run"
-        #calculate required wheel angles, speeds
+        # calculate required wheel angles, speeds
         r = rospy.Rate(60)
-        #continue endlessly
+        # continue endlessly
         while not rospy.is_shutdown():
-            #update time
+            # update time
             self.clock = time.time()
-            #write the current speeds to file
+            # write the current speeds to file
             self.recordSpeeds()
             r.sleep()
     
@@ -88,10 +87,10 @@ class GenerateReport():
     def close(self):
         self.logRosout.close()
         print"called close"
-        #need to create graph for each relation we want to display
-        self.createGraph(1,1,2,"Time (s)", "Linear Speed (m/s)","Linear")
+        # need to create graph for each relation we want to display
+        self.createGraph(1, 1, 2, "Time (s)", "Linear Speed (m/s)", "Linear")
         print "done graph 1"
-        self.createGraph(1,1,3,"Time (s)", "Angular Speed (rad/s)","Angular")
+        self.createGraph(1, 1, 3, "Time (s)", "Angular Speed (rad/s)", "Angular")
         self.document.close()
         print "exit"
 
@@ -102,4 +101,4 @@ if __name__=="__main__":
     graphGen.run()
     rospy.spin()
     print "Clean --------  ~     "
-   
+
