b15737b924b02d46dcbfcdd01d558d2d3695c023
mdtmc <miles.cranmer@gmail.com>
2015-05-11 00:07:15 -0400

    Created equations for computing the output voltage

--- catkin_ws/src/control_systems/src/pi.cpp
+++ catkin_ws/src/control_systems/src/pi.cpp
@@ -9,6 +9,8 @@ int main()
 	//The two parameters for the PI loop
 	float kp = 1;
 	float ki = 1;
+	//Refresh rate of the PI loop
+	int frequency = 100;
 	//PI loop is v(t) = kp*error(t) + ki*integral(error(x),(x,0,t))
 	//optimization will occur by changing kp, ki.
 	//The sum of all the errors:
@@ -16,15 +18,20 @@ int main()
 	//The voltage to output at each loop
 	float outputVoltage = 0;
 	//The current position of the motor		
+	//This value would be obtained from the encoders
 	float realPos;
 	//The desired position of the motor
+	//This value would be obtained from a ros topic directed from the user
 	float pos;
 	//The current error of the motor
 	float error;
-
+	//Period is time interval between refreshes
+	float period = 1/( (float)frequency )
 	while (TRUE)
 	{
-
+		error = pos - realPos;
+		intError += error*period;
+		outputVoltage = kp*error + ki*intError;
 	}
 	return 0;
 }
