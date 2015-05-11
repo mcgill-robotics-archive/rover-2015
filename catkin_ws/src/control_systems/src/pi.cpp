94a291045b4e5df0150a588053f3f47eda8d3c9f
mdtmc <miles.cranmer@gmail.com>
2015-05-10 23:55:03 -0400

    Created used variables of PI loop

--- catkin_ws/src/control_systems/src/pi.cpp
+++ catkin_ws/src/control_systems/src/pi.cpp
@@ -6,8 +6,25 @@ using namespace std;
 
 int main()
 {
+	//The two parameters for the PI loop
 	float kp = 1;
 	float ki = 1;
-	cout << "Hello World!" << endl;
+	//PI loop is v(t) = kp*error(t) + ki*integral(error(x),(x,0,t))
+	//optimization will occur by changing kp, ki.
+	//The sum of all the errors:
+	float intError = 0;
+	//The voltage to output at each loop
+	float outputVoltage = 0;
+	//The current position of the motor		
+	float realPos;
+	//The desired position of the motor
+	float pos;
+	//The current error of the motor
+	float error;
+
+	while (TRUE)
+	{
+
+	}
 	return 0;
 }
