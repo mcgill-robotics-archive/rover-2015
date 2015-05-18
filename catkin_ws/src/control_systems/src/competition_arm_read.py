bf3ebda39d00afbcfcb32047c6885f1b32d79ce4
mdtmc <miles.cranmer@gmail.com>
2015-05-18 19:00:15 -0400

    Righted circle tested function

--- catkin_ws/src/control_systems/src/competition_arm_read.py
+++ catkin_ws/src/control_systems/src/competition_arm_read.py
@@ -101,10 +101,10 @@ class ArmControlReader(object):
             #This has been optimized using Mathematica.
             points = self.circlePoints((msg.x,msg.y))
             #Corners may also be extremum
-            points.append(self.topCorner)
-            points.append(self.rightCorner)
-            points.append(self.bottomCorner)
-            points.append(self.leftCorner)
+           # points.append(self.topCorner)
+           # points.append(self.rightCorner)
+           # points.append(self.bottomCorner)
+           # points.append(self.leftCorner)
             #print points[:2]
             #Find the nearest valid point
             s = [ddistance(points[0],(msg.x,msg.y)),points[0]]
@@ -166,7 +166,7 @@ class ArmControlReader(object):
         #new test code
         v = (x-a,y-b)
         #normalize
-        av = distance(v)
+        av = distance(v[0],v[1])
         return (a+r*v[0]/av,b+r*v[1]/av)
 
     #Checks if value is inside curved region (see images)
