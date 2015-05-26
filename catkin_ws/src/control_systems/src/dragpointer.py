c7126a68726bf85ca9c8d940aea300e7d0452661
mdtmc <miles.cranmer@gmail.com>
2015-05-26 17:39:16 -0600

    Adding objects on pointer, calculating x,y position

--- catkin_ws/src/control_systems/src/dragpointer.py
+++ catkin_ws/src/control_systems/src/dragpointer.py
@@ -3,10 +3,14 @@
 import pygame
 from pygame.locals import *
 
+
 def main():
+	maxX,maxY = 500,500
+	msgx,msgy = 0,0
+	x,y = 0,0
 	#init screen
 	pygame.init()
-	screen = pygame.display.set_mode((500,500))
+	screen = pygame.display.set_mode((maxX,maxY))
 	pygame.display.set_caption('Arm Control')
 
 	#fill background
@@ -15,8 +19,10 @@ def main():
 	background.fill((250, 250, 250))
 
 	# Blit everything to the screen
-	screen.blit(background, (0, 0))
+	screen.blit(background,(0, 0))
 	pygame.display.flip()
+
+	#Draw bounds by applying same check to pixels as with
 
 	# Event loop
 	while 1:
@@ -25,9 +31,14 @@ def main():
 				return
 		screen.blit(background, (0, 0))
 		pygame.display.flip()
+		#update position of box
 		if pygame.mouse.get_pressed()[0]:
-			print pygame.mouse.get_pos()
-
+			(msgx,msgy) = pygame.mouse.get_pos()
+			(x,y) = (2*(float(msgx)-20.)/maxX,2.-4*msgy/float(maxY))
+			print (x,y)
+		screen.fill((250,250,250))
+		pygame.draw.rect(background,(0,0,0),
+			pygame.Rect(msgx-5,msgy-5,10,10))
 
 if __name__ == '__main__': 
 	main()
