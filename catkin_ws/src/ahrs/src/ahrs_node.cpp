55f96ac11061eeedb64c0abf9928e65d8fd91ee5
David Lavoie-Boutin <lavoie-boutin.david@ireq.ca>
2015-07-21 13:12:44 -0400

    imcluded ahrs library from ireq dev

---
+++ catkin_ws/src/ahrs/src/ahrs_node.cpp
@@ -0,0 +1,8 @@
+//
+// Created by david on 7/21/15.
+//
+
+#include "ahrs_node.h"
+#include "ros/ros.h"
+
+//#include "ahrs/AhrsStatus.h"
---
+++ catkin_ws/src/ahrs/src/ahrs_node.h
@@ -0,0 +1,14 @@
+//
+// Created by david on 7/21/15.
+//
+
+#ifndef PROJECT_AHRS_NODE_H
+#define PROJECT_AHRS_NODE_H
+
+
+class ahrs_node {
+
+};
+
+
+#endif //PROJECT_AHRS_NODE_H
