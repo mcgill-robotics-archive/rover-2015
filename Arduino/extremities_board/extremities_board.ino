f12e6682743557189606f2b7bb9f1b18a5f62dc1
Steven D <steven.nfd@gmail.com>
2015-07-24 21:39:03 -0400

    Basic control for all arm motors.

--- Arduino/extremities_board/extremities_board.ino
+++ Arduino/extremities_board/extremities_board.ino
@@ -1,61 +1,105 @@
-// Code for the Arduino Mega on the extremities board.
-
-#include <Servo.h>
-
-void setup()
-{
-}
-
-void loop()
-{
-  setElbowVelocity(-50);
-}
-
 /*
+    Code for the Arduino Mega on the extremities board.
+    
     HOW TO CONNECT THE MOTORS:
     
-  Linear actuators:
+  Linear actuators (up +, down -):
     (Elbow)
       -Green: M2B
       -Black: M2A
     (Shoulder)
       -White: M1B
       -Red: M1A
-  Banebot: TO BE DETERMINED
+      
+  Banebot: TO BE DETERMINED (up + down -)
     (Wrist)
-      -
-      -
+      -Black: M1B
+      -White: M1A
+      
+  Large stepper (open +, close -):
+    (Claw)
+      -Black: B-
+      -Green: B+
+      -Red: A-
+      -White: A+
+      
+  Wrist Servos (servo ports 0 and 1) (cw +, ccw -):
+    (Wrist roration) POR
+      -Black to Black
+      -Red to Red
+      -White to Black
+      -Greem to Red
 */
+
+#include <Servo.h>
+
+int _EN_CLAW = 23; // ACTIVE LOW
+int STEP_CLAW = 25;
+int DIR_CLAW = 27;
+int WRIST = 2;
+int SHOULDER = 4;
+int ELBOW = 5;
+int WRIST_ROT_1 = 10; // Servo port 0
+int WRIST_ROT_2 = 9; // Servo port 1
+  
+  
+void setup()
+{
+  Serial.begin(9600); // WHY DO I NEED THIS???
+  
+  pinMode(_EN_CLAW, OUTPUT);
+  pinMode(STEP_CLAW, OUTPUT);
+  pinMode(DIR_CLAW, OUTPUT);
+  
+  digitalWrite(_EN_CLAW, LOW); // Enables claw
+}
+
+void loop()
+{
+  setServoVelocity(100, WRIST);
+}
 
 void setClawDisplacement(int disp)
 {
+  if(disp > 0)
+  {
+    digitalWrite(DIR_CLAW, LOW);
+  }
+  else if(disp < 0)
+  {
+    disp *= -1;
+    digitalWrite(DIR_CLAW, HIGH);
+  }
+  else{}
+  for(int i = 0; i < disp * 200; i++)
+  {
+    digitalWrite(STEP_CLAW, LOW);
+    delayMicroseconds(1000);
+    digitalWrite(STEP_CLAW, HIGH);
+    delayMicroseconds(1000);
+  }
 }
 
 void setWristRotationVelocity(int vel)
 {
+  setServoVelocity(vel, WRIST_ROT_1);
+  setServoVelocity(vel, WRIST_ROT_2);
 }
 
-void setWristVelocity(int vel) // Sets the velocity of the shoulder link. -100 is the max speed down and 100 is the max speed up.
+void setServoVelocity(int vel, int pin)
 {
-  setServoVelocity(vel, 3);
-}
-
-// --------- COMPLETED vvv -------------//
-
-void setShoulderVelocity(int vel) // Sets the velocity of the shoulder link. -100 is the max speed down and 100 is the max speed up.
-{
-  setServoVelocity(vel, 4);
-}
-
-void setElbowVelocity(int vel) // Sets the velocity of the elbow link. -100 is the max speed down and 100 is the max speed up.
-{
-  setServoVelocity(vel, 5);
-}
-
-void setServoVelocity(int vel, int pin) // Sets the velocity of a servo. 0 stops the motor. -100 is the max speed in one direction and 100 is the max speed in the other.
-{
-  int minim = 30; // Minimum and maximum values for servo.write(). Experimentally determined.
-  int maxim = 150;
+  int minim;
+  int maxim;
+  if(pin == WRIST || pin == SHOULDER || pin == ELBOW)
+  {
+    minim = 30;
+    maxim = 150;
+  }
+  else
+  {
+    minim = 0;
+    maxim = 180;
+  }
   Servo servo;
   servo.attach(pin);
   if(vel == 0 || vel > 100 || vel < -100)
@@ -66,7 +110,6 @@ void setServoVelocity(int vel, int pin) // Sets the velocity of a servo. 0 stops
   {
     vel *= -1;
     int servoVal = ceil(vel * (90 - minim) / 100);
-    Serial.println(servoVal);
     servo.write(servoVal);
   }
   else
